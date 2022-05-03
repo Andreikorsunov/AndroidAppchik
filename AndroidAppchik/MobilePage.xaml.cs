e6411bb6ce9668c0e8fefd44540a348a1b35e57a
Andrey <andrei.korshunov2004@gmail.com>
2022-05-03 10:54:51 +0300

    Telefonid

---
+++ AndroidAppchik/MobilePage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidAppchik
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MobilePage : ContentPage
+    {
+        public ObservableCollection<Telefon> telefons { get; set; }
+        Label lbl_list;
+        ListView list;
+        public MobilePage()
+        {
+            telefons = new ObservableCollection<Telefon>
+            {
+                new Telefon {Nimetus="Samsung Galaxy S22", Tootja ="Samsung", Hind=1249, Pilt ="s22.png" },
+                new Telefon {Nimetus="Sony XA1", Tootja ="Sony", Hind=250, Pilt ="xa1.png" },
+                new Telefon {Nimetus="Xiaomi Mi 11 Lite 5G", Tootja ="Xiaomi", Hind=339, Pilt ="lite11.png" },
+                new Telefon {Nimetus="iPhone 13", Tootja ="Apple", Hind=1179, Pilt ="iphone.png" }
+            };
+            lbl_list = new Label
+            {
+                Text = "Telefonide loetelu",
+                HorizontalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            };
+            list = new ListView
+            {
+                HasUnevenRows = true,
+                ItemsSource = telefons,
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    ImageCell imageCell = new ImageCell { TextColor = Color.Red, DetailColor = Color.Green };
+                    imageCell.SetBinding(ImageCell.TextProperty, "Nimetus");
+                    Binding companyBinding = new Binding { Path = "Tootja", StringFormat = "Tore telefon firmalt {0}" };
+                    imageCell.SetBinding(ImageCell.DetailProperty, companyBinding);
+                    imageCell.SetBinding(ImageCell.ImageSourceProperty, "Pilt");
+                    return imageCell;
+                })
+            };
+            list.ItemTapped += List_ItemTapped;
+            this.Content = new StackLayout { Children = { lbl_list, list } };
+        }
+        private async void List_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Telefon selectedPhone = e.Item as Telefon;
+            if (selectedPhone != null)
+                await DisplayAlert("Valitud mudel", $"{selectedPhone.Tootja} -{selectedPhone.Nimetus}", "OK");
+        }
+        private void List_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+                lbl_list.Text = e.SelectedItem.ToString();
+        }
+    }
+}
