f57c6bf659ea2333e54a5f2b0a4f74cc46aa4b9c
Andrey <andrei.korshunov2004@gmail.com>
2022-05-03 12:27:44 +0300

    riik

---
+++ AndroidAppchik/Riigid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidAppchik
+{
+    public class Riigid
+    {
+        public string Riik { get; set; }
+        public string Pealinn { get; set; }
+        public int Rahvaarv { get; set; }
+        public string Flag { get; set; }
+    }
+}
