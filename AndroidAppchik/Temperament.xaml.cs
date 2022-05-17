dcc11bbe1a4bd3d4cef93b329d429ede69b2d2bb
opilane <opilane@HP-CZC5261P8F>
2022-05-17 11:21:40 +0300

    bab

---
+++ AndroidAppchik/Temperament.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidAppchik
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Temperament : ContentPage
+    {
+
+    }
+}
